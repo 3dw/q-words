707d0f4b67bb98fb0e55ea1831c3d5f04f408612
Bestian Tang <bestian@gmail.com>
2020-05-15 21:08:48 +0800

    change icon

--- src/components/win.vue
+++ src/components/win.vue
@@ -3,7 +3,6 @@
     <div class = "page">
       <div class="ui image card">
         <img src="../assets/th.jpg" />
-        <h1 class="text-white shadow">{{ $t('good!') }}</h1>
       </div>
     </div>
   </div>
