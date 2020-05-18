305b26385b2223e72bc481d63e0211379ca2c55c
Bestian Tang <bestian@gmail.com>
2020-05-18 12:50:25 +0800

    build

--- .quasar/client-entry.js
+++ .quasar/client-entry.js
@@ -10,8 +10,6 @@
  * Boot files are your "main.js"
  **/
 
-import 'quasar/dist/quasar.ie.polyfills.js'
-
 
 
 import '@quasar/extras/roboto-font/roboto-font.css'
@@ -34,9 +32,6 @@ import Vue from 'vue'
 import createApp from './app.js'
 
 
-import 'app/src-pwa/register-service-worker.js'
-
-
 
 
 import qboot_Booti18n from 'boot/i18n'
@@ -47,14 +42,15 @@ import qboot_Booti18n from 'boot/i18n'
 
 
 
+Vue.config.devtools = true
+Vue.config.productionTip = false
 
 
 
+console.info('[Quasar] Running CORDOVA.')
 
-// Needed only for iOS PWAs
-if (/iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream && window.navigator.standalone) {
-  import(/* webpackChunkName: "fastclick"  */ '@quasar/fastclick')
-}
+
+
 
 
 async function start () {
@@ -109,11 +105,16 @@ async function start () {
     
 
     
+    document.addEventListener('deviceready', () => {
+    Vue.prototype.$q.cordova = window.cordova
+    
 
     
       new Vue(app)
     
 
+    
+    }, false) // on deviceready
     
 
     
--- quasar.conf.js
+++ quasar.conf.js
@@ -154,7 +154,7 @@ module.exports = function (ctx) {
 
     cordova: {
        id: 'tw.bestian.word',
-       version: '1.0.6',
+       version: '1.0.9',
        noIosLegacyBuildFlag: true, // uncomment only if you know what you are doing
     },
 
