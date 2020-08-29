b83e2a4499c806ae0a4c533dbb0094679eff14e4
Bestian Tang <bestian@gmail.com>
2020-08-30 07:27:35 +0800

    test

--- quasar.conf.js
+++ quasar.conf.js
@@ -154,7 +154,7 @@ module.exports = function (ctx) {
 
     cordova: {
        id: 'tw.bestian.word',
-       version: '1.0.10',
+       version: '1.1.2',
        noIosLegacyBuildFlag: true, // uncomment only if you know what you are doing
     },
 
