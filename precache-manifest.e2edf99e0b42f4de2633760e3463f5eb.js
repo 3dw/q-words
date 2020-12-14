1a9ed5eeaf82be84641bd043bffcdeaaefb5c2f7
Bestian Tang <bestian@gmail.com>
2020-12-14 18:00:49 +0800

    deploy

---
+++ precache-manifest.e2edf99e0b42f4de2633760e3463f5eb.js
@@ -0,0 +1,190 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad1f17432cf672168f31",
+    "url": "css/10.eb6b72c9.css"
+  },
+  {
+    "revision": "2554fb482e4acc8c7980",
+    "url": "css/2.51593271.css"
+  },
+  {
+    "revision": "2f05a3d2b0a9115007b5",
+    "url": "css/3.fbfc200a.css"
+  },
+  {
+    "revision": "3a633661f20256c0d461",
+    "url": "css/4.927d4902.css"
+  },
+  {
+    "revision": "eb3cecdafac145d663e0",
+    "url": "css/5.6e3961ac.css"
+  },
+  {
+    "revision": "595ceef92aa1ba2c0b18",
+    "url": "css/6.b7ed41c2.css"
+  },
+  {
+    "revision": "2dfb7f2613e7b9564fb6",
+    "url": "css/7.397d563d.css"
+  },
+  {
+    "revision": "9d01c2dd8c59a52122a5",
+    "url": "css/8.eb73a987.css"
+  },
+  {
+    "revision": "c12bcaf9cdbdac223cea",
+    "url": "css/9.1e69578d.css"
+  },
+  {
+    "revision": "021f73d10e481780c212",
+    "url": "css/app.c2b03bec.css"
+  },
+  {
+    "revision": "5cb7edfceb233100075dc9a1e12e8da3",
+    "url": "fonts/KFOkCnqEu92Fr1MmgVxIIzQ.a45108d3.woff"
+  },
+  {
+    "revision": "87284894879f5b1c229cb49c8ff6decc",
+    "url": "fonts/KFOlCnqEu92Fr1MmEU9fBBc-.cea99d3e.woff"
+  },
+  {
+    "revision": "b00849e00f4c2331cddd8ffb44a6720b",
+    "url": "fonts/KFOlCnqEu92Fr1MmSU5fBBc-.865f928c.woff"
+  },
+  {
+    "revision": "adcde98f1d584de52060ad7b16373da3",
+    "url": "fonts/KFOlCnqEu92Fr1MmWUlfBBc-.2267169e.woff"
+  },
+  {
+    "revision": "bb1e4dc6333675d11ada2e857e7f95d7",
+    "url": "fonts/KFOlCnqEu92Fr1MmYUtfBBc-.bac8362e.woff"
+  },
+  {
+    "revision": "60fa3c0614b8fb2f394fa29944c21540",
+    "url": "fonts/KFOmCnqEu92Fr1Mu4mxM.49ae34d4.woff"
+  },
+  {
+    "revision": "84a37de85c17f186652a179b1145392f",
+    "url": "fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNa.1811d381.woff"
+  },
+  {
+    "revision": "703cf8f274fbb265d49c6262825780e1",
+    "url": "fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.fa3334fe.woff2"
+  },
+  {
+    "revision": "c8aad7e0fc951d7ede2817faee528a71",
+    "url": "index.html"
+  },
+  {
+    "revision": "ad1f17432cf672168f31",
+    "url": "js/10.ad1f1743.js"
+  },
+  {
+    "revision": "5bbb064323314cbfdd3a",
+    "url": "js/11.5bbb0643.js"
+  },
+  {
+    "revision": "cdf211e1f2d903584925",
+    "url": "js/12.cdf211e1.js"
+  },
+  {
+    "revision": "2554fb482e4acc8c7980",
+    "url": "js/2.2554fb48.js"
+  },
+  {
+    "revision": "2f05a3d2b0a9115007b5",
+    "url": "js/3.2f05a3d2.js"
+  },
+  {
+    "revision": "3a633661f20256c0d461",
+    "url": "js/4.3a633661.js"
+  },
+  {
+    "revision": "eb3cecdafac145d663e0",
+    "url": "js/5.eb3cecda.js"
+  },
+  {
+    "revision": "595ceef92aa1ba2c0b18",
+    "url": "js/6.595ceef9.js"
+  },
+  {
+    "revision": "2dfb7f2613e7b9564fb6",
+    "url": "js/7.2dfb7f26.js"
+  },
+  {
+    "revision": "9d01c2dd8c59a52122a5",
+    "url": "js/8.9d01c2dd.js"
+  },
+  {
+    "revision": "c12bcaf9cdbdac223cea",
+    "url": "js/9.c12bcaf9.js"
+  },
+  {
+    "revision": "021f73d10e481780c212",
+    "url": "js/app.313b052c.js"
+  },
+  {
+    "revision": "e924a941985583451f03",
+    "url": "js/vendor.e924a941.js"
+  },
+  {
+    "revision": "79ac7da7c81551e19b0e431a891541e5",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "fe53608190d713b2c111f2caa795bba8",
+    "url": "statics/=成語.json"
+  },
+  {
+    "revision": "05912c3707c03b7fa490e0dba0a03b32",
+    "url": "statics/=歇後語.json"
+  },
+  {
+    "revision": "3a591e8fdbd980c8d533c9cef1245aee",
+    "url": "statics/=無法考證者.json"
+  },
+  {
+    "revision": "2402155ff85fa2f53d2f197beb9fdb3c",
+    "url": "statics/=義譯.json"
+  },
+  {
+    "revision": "964b9799e69ca872868e79fc9f90d8fd",
+    "url": "statics/=諺語.json"
+  },
+  {
+    "revision": "7066a3e78ec05df02bd360626a3c7f66",
+    "url": "statics/=音義合譯.json"
+  },
+  {
+    "revision": "1b18ad99859419ff685f06fe3434e684",
+    "url": "statics/=音譯.json"
+  },
+  {
+    "revision": "330cada20292a01d1df39a5592d323ad",
+    "url": "statics/a/index.json"
+  },
+  {
+    "revision": "3a2d4e4a7e84f89ce2b1b1e441919e2c",
+    "url": "statics/c/index.json"
+  },
+  {
+    "revision": "f3f89beb0d9fe16be5613b57975d4f61",
+    "url": "statics/favicon.ico"
+  },
+  {
+    "revision": "14246b6fdced42f83dcee48878aa9267",
+    "url": "statics/h/index.json"
+  },
+  {
+    "revision": "93beabcfb74f65de288a7519d11f7473",
+    "url": "statics/icons/favicon.ico"
+  },
+  {
+    "revision": "435c4d6ea5257136f380ef21901eccb8",
+    "url": "statics/manifest-hosted.app"
+  },
+  {
+    "revision": "66a400e8419196866b4491af59a5aa13",
+    "url": "statics/t/index.json"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,36 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "precache-manifest.e2edf99e0b42f4de2633760e3463f5eb.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "q-words"});
+
+workbox.core.skipWaiting();
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("index.html"));
+
+workbox.routing.registerRoute(/.(?:png|jpg|jpeg|svg|txt)$/, new workbox.strategies.CacheFirst({ "cacheName":"images", plugins: [] }), 'GET');
